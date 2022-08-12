7dd34817d939b5c55a63185cb50626fc57a625b1
ashokkanigiri <ashkanigiri@gmail.com>
2022-08-12 06:20:07 +0530

    Added hilt deps & setup

---
+++ app/src/main/java/com/ashok/kanigiri/flipkartclone/FlipkartCloneApplication.kt
@@ -0,0 +1,13 @@
+package com.ashok.kanigiri.flipkartclone
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FlipkartCloneApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
